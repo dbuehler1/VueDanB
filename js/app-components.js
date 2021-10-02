f89f142148495581dd6e8f77aced689fb5944677
Daniel <dbuehler1@my.wctc.edu>
2021-10-02 14:02:44 -0500

    Updated Version

--- js/app-components.js
+++ js/app-components.js
@@ -1,7 +1,7 @@
-Vue.component('List', {
+Vue.component('InventoryList', {
 
     props: {
-        name:{
+        name: {
             type: String,
         },
         items: {
@@ -13,15 +13,15 @@ Vue.component('List', {
                 <h3>{{name}}</h3>
                 <ul class="list-group list-group-flush">
                     <list-item v-for="(item, i) in items" 
-                    :item="item" 
+                    :item="item"
+                    :name="item.name" 
                     :key="item.name"
-                    @remove-item="$emit('remove-item', item)"
                     ></list-item>
                 </ul>
             </div>`,
 });
-Vue.component('ListItem' , {
-    data: function(){
+Vue.component('ListItem', {
+    data: function () {
         return {
             uid: Math.floor(Math.random() * 10e16),
         }
@@ -34,30 +34,95 @@ Vue.component('ListItem' , {
         },
     },
     methods: {
+        popModal(){
+            console.log(this.item);
 
+            // this.testName = this.item.name;
+
+            $('#itemNameLabel').val(this.item.name);
+            $('#itemQty').val(this.item.qty);
+            $('#itemDescLabel').val(this.item.description);
+            $('#itemCategory').val(this.item.category);
+        },
+
+        applyTo(){
+            console.log(this.item);
+
+            app.inventory.indexOf(this.item).name = $('#itemNameLabel').val();
+            // this.item.name = $('#itemNameLabel').val();
+            this.item.qty = $('#itemQty').val();
+            this.item.description = $('#itemDescLabel').val();
+            this.item.category = $('#itemCategory').val();
+        },
+        deleteRecord(){
+            app.inventory.splice(app.inventory.indexOf(this.item), 1);
+        },
     },
 
-    template: `
+    template: `            
             <div class="itemsTable row">
+            <div class="modal fade" id="changeItemModal" tabindex="-1" role="dialog" aria-labelledby="editItemModal" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <form>
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="editItemModal">Edit Item</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <label for="itemNameLabel">Item Name</label>
+                        <input id="itemNameLabel" type="text" class="form-control">
+                        
+
+                        <label for="itemQty">Item Qty</label>
+                        <input id="itemQty" type="text" class="form-control">
+
+                        <label for="itemDescLabel">Item Description</label>
+                        <input id="itemDescLabel" type="text" class="form-control">
+
+                        <label for="itemCategory">Item Category</label>
+                        <input id="itemCategory" type="text" class="form-control">
+
+                    </div>
+                    <div class="modal-footer">
+                        <b-button variant="danger" type="button" data-dismiss="modal">Close</b-button>
+                        <b-button variant="success" type="button" @click="applyTo" data-dismiss="modal">Apply</b-button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+            <div class="col-2">
+                <b-button variant="success" type="button" data-toggle="modal" @click="popModal" data-target="#changeItemModal" title="Add Item">
+                    <i class="far fa-edit"></i>
+                </b-button>
+                <b-button variant="danger" @click="deleteRecord" type="button" >
+                    <i class="fas fa-trash-alt"></i>
+                </b-button>
+                
+            </div>
             <div class="col-3">
                 {{ item.name }}
             </div>
-            <div class="col-3">
+            <div class="col-2">
             {{ item.category }}
             </div>
             <div class="col-4">
             {{ item.description }}
             </div>
-            <div class="col-2">
+            <div class="col-1">
             {{ item.qty }}
             </div>
+            
             <hr>
             </div>`
 })
-Vue.component('searchList', {
+Vue.component('SearchList', {
 
     props: {
-        name:{
+        name: {
             type: String,
         },
         items: {
@@ -67,30 +132,28 @@ Vue.component('searchList', {
     },
     template: `<div class="my-search-inventory">
                 <h3>{{name}}</h3>
-                <ul class="list-group list-group-flush">
-                    <list-item v-for="(item, i) in items" 
+                <ul  class="list-group list-group-flush">
+                    <search-list-item v-for="(item, i) in items" 
                     :item="item" 
                     :key="item.name"
                     @remove-item="$emit('remove-item', item)"
-                    ></list-item>
+                    ></search-list-item>
                 </ul>
             </div>`,
 });
-Vue.component('SearchList' , {
-    data: function(){
+Vue.component('SearchListItem', {
+    data: function () {
         return {
             uid: Math.floor(Math.random() * 10e16),
         }
     },
     props: {
         item: {
-            type: Array,
+            type: Object,
             required: true,
         },
     },
-    methods: {
-
-    },
+    methods: {},
 
     template: `
             <div class="itemsTable row">
@@ -106,6 +169,65 @@ Vue.component('SearchList' , {
             <div class="col-2">
             {{ item.qty }}
             </div>
+            
             <hr>
             </div>`
 });
+
+Vue.component('EnterInfo', {
+    props: {
+        newEntry: {
+            name: '',
+            description: '',
+            qty: 1,
+            category: '',
+        },
+
+        items: {
+            type: Array,
+            required: true,
+        },
+    },
+    methods: {
+        addIt: function (e) {
+            this.items.push(this.newEntry);
+
+            this.newEntry = {
+                name: '',
+                description: '',
+                qty: 1,
+                category: '',
+            };
+        },
+    },
+
+    template: `<form>
+                <div class="enterItems">
+                    <div class="form-group">
+                        <label for="name">Item Name</label>
+                        <input id="name"  type="text" class="form-control" v-model="newEntry.name" >
+                    </div>
+                    <div class="form-group">
+                        <label for="qty">Quantity</label>
+                        <input id="qty" type="number" class="form-control" size="3" v-model="newEntry.qty">
+                    </div>
+                    <div class="form-group">
+                        <label for="desc">Description</label>
+                        <input id="desc" type="text" class="form-control" v-model="newEntry.description">
+                    </div>
+                    <div class="form-group">
+                        <label for="category">Category</label>
+                        <input id="category" type="text" class="form-control" v-model="newEntry.category">
+                    </div>
+                </div>
+                <br>
+                <b-button variant="success" id="addContent" @click="addIt">Add +</b-button><br><br>
+                    <span id="addItemError"></span>
+
+            </form>`
+});
+
+
+
+
+
--- js/app.js
+++ js/app.js
@@ -25,113 +25,7 @@ const app = new Vue({
         }
     },
     methods: {
-        addIt: function (e) {
-            $('#addItemError').text('');
-            console.log($('#name').val().length < 1);
-            //checking for blank fields
-            if (!$('#name').val().length < 1 && !$('#category').val().length < 1 && !$('#qty').val().length < 1 && !$('#desc').val().length < 1) {
-                console.log(this)
-                var isDuplicate = false;
-                //checking for duplicate entries
-                this.inventory.forEach(function (item) {
-                    if (item.name.toLowerCase() === $('#name').val().toLowerCase()) {
-                        isDuplicate = true;
-                    }
-                })
-                console.log(isDuplicate);
-                //if duplicate entry is found, no new entry will be pushed
-                if (!isDuplicate === true) {
-                    //remove potential previous error message
-                    $('#addItemError').text('');
-                    this.inventory.push(this.newItem);
 
-                    this.newItem = {
-                        name: '',
-                        description: '',
-                        qty: 1,
-                        category: '',
-                    };
-                } else {
-                    //provide error details for user
-                    $('#addItemError').text('Item with name: \"' + $('#name').val() + '\" already exists');
-                }
-
-            }
-            else{
-                $('#addItemError').text('Please fill out all fields');
-            }
-
-
-        },
-        populateModal(){
-
-            var editableRecord = false;
-            this.inventory.forEach(function (item) {
-                if ($('#nameIdentifier').val().toLowerCase() === item.name.toLowerCase() &&
-                    $('#categoryIdentifier').val().toLowerCase() === item.category.toLowerCase()
-                ) {
-                    $('#itemNameLabel').val(item.name);
-                    $('#itemQty').val(item.qty);
-                    $('#itemDescLabel').val(item.description);
-                    $('#itemCategory').val(item.category);
-                    editableRecord = true;
-                }
-            })
-
-            if (editableRecord === true) {
-                $('#deleteItemError').text('');
-
-            } else {
-                $('#deleteItemError').text('Record: \"' + $('#nameIdentifier').val() + ', '
-                    + $('#categoryIdentifier').val() + '\" could not be found');
-            }
-
-        },
-
-        applyChanges(){
-            this.inventory.forEach(function (item) {
-                if ($('#nameIdentifier').val().toLowerCase() === item.name.toLowerCase() &&
-                    $('#categoryIdentifier').val().toLowerCase() === item.category.toLowerCase()
-                ) {
-                    item.name = $('#itemNameLabel').val();
-                    item.qty = $('#itemQty').val();
-                    item.description = $('#itemDescLabel').val();
-                    item.category = $('#itemCategory').val();
-
-                }
-            })
-        },
-
-        deleteRecord() {
-            var foundRecord = false;
-            this.inventory.forEach(function (item) {
-                if ($('#nameIdentifier').val().toLowerCase() === item.name.toLowerCase() &&
-                    $('#categoryIdentifier').val().toLowerCase() === item.category.toLowerCase()
-                ) {
-                    app.inventory.splice(app.inventory.indexOf(item), 1);
-                    foundRecord = true;
-                }
-            })
-
-            //removing item from both the search results and normal table
-            app.searchinventory.forEach(function (item) {
-                if ($('#nameIdentifier').val().toLowerCase() === item.name.toLowerCase() &&
-                    $('#categoryIdentifier').val().toLowerCase() === item.category.toLowerCase()
-                ) {
-                    app.searchinventory.splice(app.searchinventory.indexOf(item), 1);
-                    foundRecord = true;
-                }
-            })
-
-            if (foundRecord === true) {
-                $('#deleteItemError').text('');
-
-            } else {
-                $('#deleteItemError').text('Record: \"' + $('#nameIdentifier').val() + ', '
-                    + $('#categoryIdentifier').val() + '\" could not be found');
-            }
-
-        },
         search() {
             $('.searchTableHeader').text('Search Results for: \"' + $('#searchBox').val() + "\"")
             app.searchinventory = [];
@@ -142,6 +36,7 @@ const app = new Vue({
                     if (item.name.toLowerCase().includes($('#searchBox').val().toLowerCase())) {
                         console.log(item);
                         console.log(app.searchinventory);
+
                         app.searchinventory.push(item);
 
 
@@ -187,11 +82,11 @@ const app = new Vue({
 
     },
     computed: {
-        myInventory: function () {
-            return this.inventory.filter(function (item) {
-                return item;
-            });
-        },
+        // myInventory: function () {
+        //     return this.inventory.filter(function (item) {
+        //         return item;
+        //     });
+        // },
 
     },
     // mounted: function() {
